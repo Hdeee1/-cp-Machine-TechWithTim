7184066e4c7804e778312d3f64bfbbee1cbbb593
Hadyu <hadyuhakim@gmail.com>
2024-12-31 09:15:07 +0700

    collect a bet amount

--- project.js
+++ project.js
@@ -15,38 +15,38 @@ const deposit = () => {
 
     if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
       console.log("Invalid deposit amount. Please try again!");
-    }else{
-        return numberDepositAmount;
+    } else {
+      return numberDepositAmount;
     }
   }
 };
 
 const getNumberOfLines = () => {
-    while (true) {
-        const lines = prompt("Enter the number of lines to bet on (1-3): ");
-        const numberOfLines = parseFloat(lines);
-    
-        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
-          console.log("Invalid number of lines, try again!");
-        }else{
-            return numberOfLines;
-        }
-      } 
-}
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
 
-const getBet = (balance) => {
-    while (true) {
-        const bet = prompt("Enter the total bet: ");
-        const numberBet = parseFloat(bet);
-    
-        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance) {
-          console.log("Invalid bet. Please try again!");
-        }else{
-            return numberBet;
-        }
-      }
-}
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, try again!");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const getBet = (balance, lines) => {
+  while (true) {
+    const bet = prompt("Enter the bet per line: ");
+    const numberBet = parseFloat(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+      console.log("Invalid bet. Please try again!");
+    } else {
+      return numberBet;
+    }
+  }
+};
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
-const bet = getBet(balance);
+const bet = getBet(balance, numberOfLines);
